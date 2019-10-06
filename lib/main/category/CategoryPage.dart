a32fedef58603384cb644019b374913be7aa3bfa
zhengxin <zhengxin@shein.com>
2019-10-07 01:34:01 +0800

    feat（列表页）:顶部Tab切换 1，顶部Tab数据放在集合headerTabList里面，id用来设置是否选中，title显示标题，fileds,sort接口使用 2，遍历headerTabList来展示Tab 3，切换Tab时，要重置_page,重置productList,回到顶部，调用列表接口 4，价格和销量有2个状态，通过*1来设置，箭头通过ID和sort来确定是否展示，及展示的颜色

--- lib/main/category/CategoryPage.dart
+++ lib/main/category/CategoryPage.dart
@@ -1,5 +1,4 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_jd/common/constant/ArgumentKey.dart';
 import 'package:flutter_jd/common/constant/RequestURL.dart';
--- lib/product/list/ProductListPage.dart
+++ lib/product/list/ProductListPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_jd/common/constant/ArgumentKey.dart';
 import 'package:flutter_jd/common/constant/RequestURL.dart';
 import 'package:flutter_jd/common/util/ImageUtil.dart';
+import 'package:flutter_jd/common/util/Log.dart';
 import 'package:flutter_jd/common/util/ScreenHelper.dart';
 import 'package:flutter_jd/common/widget/LodingMoreView.dart';
 import 'package:flutter_jd/main/home/bean/product_entity.dart';
@@ -19,24 +20,54 @@ class ProductListPage extends StatefulWidget {
 
 class _ProductListPageState extends State<ProductListPage> {
   final arguments;
-  var _page = 1;                                                 //页面
-  var _pageSize = "5";                                           //一页数据
+  var _page = 1; //页面
+  var _pageSize = "5"; //一页数据
   ProductEntity _productEntity;
   List<ProductResult> _productList = [];
   ScrollController _controller = ScrollController();
-  var _isRequesting = false;                                     //正在请求
-  var _hasMore = false;
-  _ProductListPageState({this.arguments});
+  var _isRequesting = false; //正在请求
   final GlobalKey<ScaffoldState> globalKey = GlobalKey<ScaffoldState>();
+  var _hasMore = false; //是否加载更多
+  var _selectTabId = 1; //Tab选中的索引ID
+  var _sort = ""; //接口里面排序
+  var _cateId = "";
+
+  var headerTabList = [
+    {
+      "id": 1, //通过ID来设置选中状态
+      "title": "综合", //标题
+      "fileds": "all", //跟sort一起组合成接口需要的排序字段
+      "sort": -1,
+    },
+    {
+      "id": 2,
+      "title": "销量",
+      "fileds": "salecount",
+      "sort": -1,
+    },
+    {
+      "id": 3,
+      "title": "价格",
+      "fileds": "price",
+      "sort": -1,
+    },
+    {
+      "id": 4,
+      "title": "筛选",
+    },
+  ];
+
+  _ProductListPageState({this.arguments});
+
   @override
   void initState() {
     super.initState();
-    var cateId = arguments["${ArgumentKey.cateId}"];
-    _getProductList(cateId);
-    _controller.addListener((){
-      if(_controller.position.pixels == _controller.position.maxScrollExtent){
-        if(_hasMore){
-          _getProductList(cateId);
+    _cateId = arguments["${ArgumentKey.cateId}"];
+    _getProductList();
+    _controller.addListener(() {
+      if (_controller.position.pixels == _controller.position.maxScrollExtent) {
+        if (_hasMore) {
+          _getProductList();
         }
       }
     });
@@ -48,9 +79,7 @@ class _ProductListPageState extends State<ProductListPage> {
       key: globalKey,
       appBar: AppBar(
         title: Text("Product List"),
-        actions: [
-          Text("")
-        ],
+        actions: [Text("")],
       ),
       endDrawer: Drawer(
         child: Text("Test Drawer"),
@@ -67,71 +96,69 @@ class _ProductListPageState extends State<ProductListPage> {
   ///顶部Tab
   Positioned _productHeaderWidget() {
     return Positioned(
-          height: 50,
-          width: ScreenHelper.screenWidthDp,
-          top: 0,
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: Colors.grey[300],
-                ),
-              ),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: InkWell(
-                    child: Container(
-                        height: double.infinity,
-                        alignment: Alignment.center,
-                        child: Text("综合")),
-                    onTap: () {},
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    child: Container(
-                      height: double.infinity,
-                      alignment: Alignment.center,
-                      child: Text("销量"),
-                    ),
-                    onTap: () {},
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    child: Container(
-                        height: double.infinity,
-                        alignment: Alignment.center,
-                        child: Text("价格")),
-                    onTap: () {},
-                  ),
-                ),
-                Expanded(
-                  child: InkWell(
-                    child: Container(
-                        height: double.infinity,
-                        alignment: Alignment.center,
-                        child: Text("筛选")),
-                    onTap: () {
-                      globalKey.currentState.openEndDrawer();
-                    },
-                  ),
-                ),
-              ],
+      height: 50,
+      width: ScreenHelper.screenWidthDp,
+      top: 0,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Colors.grey[300],
             ),
           ),
-        );
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: headerTabList.map((tab) {
+            return Expanded(
+              child: InkWell(
+                child: Container(
+                    height: double.infinity,
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          tab["title"],
+                          style: TextStyle(
+                              color: (_selectTabId == tab["id"])
+                                  ? Colors.red
+                                  : Colors.grey),
+                        ),
+                        _showTabArrow(tab["id"],tab["sort"]),
+                      ],
+                    )),
+                onTap: () {
+                  setState(() {
+                    _selectTabId = tab["id"];
+                    if (tab["id"] != 4) {
+                      _sort = "${tab["fileds"]}_${tab["sort"]}";
+                      _page = 1; //重置页码
+                      tab['sort'] = -1 * tab['sort']; //点击取反
+                      _productList = []; //重置数据
+                      _controller.jumpTo(0); //回到顶部
+                      _getProductList();
+                    } else {
+                      globalKey.currentState.openEndDrawer();
+                    }
+                  });
+                },
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
   }
 
   ///无Tab列表
-   Widget _productListWidget() {
-     if(_productEntity == null || _productEntity.result == null ||_productList.length == 0){
-       return LoadingMoreView();
-    }else{
-      return  Container(
+  Widget _productListWidget() {
+    if (_productEntity == null ||
+        _productEntity.result == null ||
+        _productList.length == 0) {
+      return LoadingMoreView();
+    } else {
+      return Container(
         margin: EdgeInsets.only(top: 50),
         child: Padding(
           padding: EdgeInsets.only(left: 10, right: 10),
@@ -175,17 +202,20 @@ class _ProductListPageState extends State<ProductListPage> {
                                         child: Text("4G"),
                                         decoration: BoxDecoration(
                                             border:
-                                            Border.all(color: Colors.grey),
+                                                Border.all(color: Colors.grey),
                                             borderRadius: BorderRadius.all(
                                                 Radius.circular(4))),
                                         padding: EdgeInsets.only(
-                                            left: 5, right: 5, top: 0, bottom: 0),
+                                            left: 5,
+                                            right: 5,
+                                            top: 0,
+                                            bottom: 0),
                                         margin: EdgeInsets.only(right: 5),
                                       )
                                     ],
                                   ),
                                 ),
-                                Text("6599.00￥")
+                                Text("${_productList[index].price}￥")
                               ],
                             ),
                           ),
@@ -205,31 +235,42 @@ class _ProductListPageState extends State<ProductListPage> {
       );
     }
   }
+
   //显示加载更多
-  StatelessWidget showMoreWidget(int index){
-    if(_hasMore){
-      return index == _productList.length -1 ? LoadingMoreView(): Text("");
-    }else{
-      return index == _productList.length -1 ? Text("加载完了..."): Text("");
+  StatelessWidget showMoreWidget(int index) {
+    if (_hasMore) {
+      return index == _productList.length - 1 ? LoadingMoreView() : Text("");
+    } else {
+      return index == _productList.length - 1 ? Text("加载完了...") : Text("");
     }
   }
 
-  void _getProductList(goodsId) async{
-    if(_isRequesting) return;
+  void _getProductList() async {
+    if (_isRequesting) return;
     _isRequesting = true;
-    var url = "${productListUrl}cid=$goodsId&page=$_page&pageSize=$_pageSize";
-    print('$url');
+    var url =
+        "${productListUrl}cid=$_cateId&page=$_page&pageSize=$_pageSize&sort=$_sort";
+    Log.d('$url');
     var response = await Dio().get(url);
     setState(() {
       _productEntity = ProductEntity.fromJson(response.data);
       _productList.addAll(_productEntity.result);
-      if(_productEntity.result.length>0){
+      if (_productEntity.result.length > 0) {
         _page++;
         _hasMore = true;
-      }else{
+      } else {
         _hasMore = false;
       }
       _isRequesting = false;
     });
+  }
+
+  ///显示Tab上的箭头
+  _showTabArrow(id,sort) {
+    if(id == 2 || id == 3){
+      return Icon(sort == -1 ? Icons.arrow_drop_down : Icons.arrow_drop_up,color: _selectTabId == id ? Colors.red : Colors.grey,);
+    }else{
+      return Text("");
+    }
   }
 }
