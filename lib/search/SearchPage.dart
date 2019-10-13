d6c8fe3360de8d04da17fe60d2ce3fba06a88a79
zhengxin <zhengxin@shein.com>
2019-10-14 00:15:29 +0800

    feat（搜索）:热门搜索和历史搜索样式完成 遇到的问题： 1，Container如果指定了alignment，宽度就会满屏

--- lib/search/SearchPage.dart
+++ lib/search/SearchPage.dart
@@ -39,10 +39,95 @@ class _SearchPageState extends State<SearchPage> {
           )
         ],
       ),
-      body: Column(
-        children: <Widget>[
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("热搜",style: TextStyle(
+              fontWeight: FontWeight.w600
+            ),),
+            Wrap(
+              crossAxisAlignment: WrapCrossAlignment.center,
+              runSpacing: 10,
+              spacing: 10,
+              children: <Widget>[
+                new TagText(text: "Mack Book",),
+                new TagText(text: "联想笔记本",),
+                new TagText(text: "小新笔记本",),
+                new TagText(text: "Mack Book",),
+                new TagText(text: "Mack Book",),
+                new TagText(text: "联想笔记本",),
+                new TagText(text: "小新笔记本",),
+                new TagText(text: "Mack Book",),
+              ],
+            ),
+            Divider(height: ScreenHelper.height(40),color: Colors.grey[300],thickness: 10,),
+            Text("历史搜索",style: TextStyle(
+              fontWeight: FontWeight.w600
+            ),),
+            Divider(height: ScreenHelper.height(1),),
+            Container(
+              alignment: Alignment.centerLeft,
+              height: ScreenHelper.height(60),
+                child: Text("女装",textAlign: TextAlign.center,)),
+            Divider(height: ScreenHelper.height(1),),
+            Container(
+                alignment: Alignment.centerLeft,
+                height: ScreenHelper.height(60),
+                child: Text("男装")),
+            Divider(height: ScreenHelper.height(1),),
+            Container(
+                alignment: Alignment.centerLeft,
+                height: ScreenHelper.height(60),
+                child: Text("笔记本")),
+            Divider(height: ScreenHelper.height(1),),
+            Container(
+                alignment: Alignment.centerLeft,
+                height: ScreenHelper.height(60),
+                child: Text("cup")),
+            InkWell(
+              onTap: (){},
+              child: Container(
+                margin: EdgeInsets.only(top: 10),
+                width: double.infinity,
+                alignment: Alignment.center,
+                height: ScreenHelper.height(70),
+                decoration: BoxDecoration(
+                  border:Border.all(color: Colors.grey[200]),
+
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.delete),
+                    Text("清空历史记录"),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class TagText extends StatelessWidget {
+  final text;
+  const TagText({
+    Key key,this.text
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 8,right: 8,top: 5,bottom: 5),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.all(Radius.circular(20))
+      ),
+      child:Text(text,textAlign: TextAlign.center,),
     );
   }
 }
